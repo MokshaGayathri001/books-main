035a294e7c54c9f56da25db7fe1826ef55d14c6b
MokshaGayathri001 <145901148+MokshaGayathri001@users.noreply.github.com>
2024-10-07 11:08:09 +0530

    editconfig

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 // PDO Connection
 try {
-    $pdo = new PDO("sqlsrv:server = tcp:it21253812abcuni.database.windows.net,1433; Database = abcuni", "azuresql", "Alpha@0515");
+    $pdo = new PDO("sqlsrv:server = tcp:it21253812abcuni.database.windows.net,1433; Database = abcuni", "azuresql", "GI@12345");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
